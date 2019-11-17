deb142b64f15a661292a57daaeed3caa4073cd1e
tiger <1165069099@qq.com>
2019-11-17 13:12:18 +0800

    docker

--- 枚举类/枚举Enum.java
+++ 枚举类/枚举Enum.java
@@ -1,36 +1,55 @@
-package cn.healthmall.sail.base.enums;
+package com.tiger.enums;
 
 import java.util.Objects;
 
-public enum GymStateEnum {
-	OPEN_FREE_DEVICE(0, "营业中且有空闲状态跑步机"),
-	OPEN_NO_DEVICE(1, "营业中且无空闲状态跑步机"),
-	CLOSE(2, "休业");
+/**
+ * XWPF 表格边框样式
+ *
+ * @author: tiger
+ * @create: 2019-11-02 12:20
+ */
+public enum XWPFBorderStyleEnum {
+    //细实线
+    single(0, "single"),
+    //厚实线
+    thick(1, "thick"),
+    //虚线
+    dashed(2, "dashed"),
+    //细虚线
+    dotted(3, "dotted"),
+    //双线
+    double1(4, "double"),
+    //波动线
+    wave(5, "wave"),
+    //无线
+    center(6, "center"),
 
-	private Integer code;
-	private String msg;
+    ;
 
-	public Integer getCode() {
-		return code;
-	}
+    private Integer code;
+    private String  msg;
 
-	public String getMsg() {
-		return msg;
-	}
+    public Integer getCode() {
+        return code;
+    }
 
-	private GymStateEnum(Integer code, String msg) {
-		this.code = code;
-		this.msg = msg;
-	}
+    public String getMsg() {
+        return msg;
+    }
 
-	public static GymStateEnum getByCode(Integer code) {
-		if (Objects.nonNull(code)) {
-			for (GymStateEnum value : GymStateEnum.values()) {
-				if (value.code == code) {
-					return value;
-				}
-			}
-		}
-		return null;
-	}
+    private XWPFBorderStyleEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static XWPFBorderStyleEnum getByCode(Integer code) {
+        if (Objects.nonNull(code)) {
+            for (XWPFBorderStyleEnum value : XWPFBorderStyleEnum.values()) {
+                if (value.code.equals(code)) {
+                    return value;
+                }
+            }
+        }
+        return null;
+    }
 }
