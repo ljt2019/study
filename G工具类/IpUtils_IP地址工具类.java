963e38c2885ba4a16eb3e44ecd7dc04294a8feb6
linjitai <linjitai@ly-sky.com>
2020-04-14 23:46:46 +0800

    utils

--- G工具类/IpUtils_IP地址工具类.java
+++ G工具类/IpUtils_IP地址工具类.java
@@ -1,8 +1,21 @@
-	public static String getIpAddress(HttpServletRequest request){
-        
+package utils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @description: 常用工具方法
+ * @author: tiger
+ * @create: 2020-04-14 22:27
+ */
+public class IpUtils {
+
+    public static String getIpAddress(HttpServletRequest request) {
+
         String ipAddress = request.getHeader("x-forwarded-for");
-        
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("Proxy-Client-IP");
         }
         if (ipAddress == null || ipAddress.length() == 0 || "unknow".equalsIgnoreCase(ipAddress)) {
@@ -10,8 +23,8 @@
         }
         if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
-            
-            if(ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){
+
+            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
                 //根据网卡获取本机配置的IP地址
                 InetAddress inetAddress = null;
                 try {
@@ -22,14 +35,18 @@
                 ipAddress = inetAddress.getHostAddress();
             }
         }
-        
+
         //对于通过多个代理的情况，第一个IP为客户端真实的IP地址，多个IP按照','分割
-        if(null != ipAddress && ipAddress.length() > 15){
+        if (null != ipAddress && ipAddress.length() > 15) {
             //"***.***.***.***".length() = 15
-            if(ipAddress.indexOf(",") > 0){
+            if (ipAddress.indexOf(",") > 0) {
                 ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
             }
         }
-        
+
         return ipAddress;
     }
+
+
+}
+
